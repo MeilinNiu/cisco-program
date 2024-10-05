321e289443625938c0a420b5cac25c92a0a635a7
“MeilinNiu” <niumeilingrace@gmail.com>
2024-10-04 20:21:20 -0700

    solve the issue of not showing anything on the website

--- sextant/src/index.js
+++ sextant/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import Board from './App';
+import Board from './App.js';
 import './index.css';
 
-ReactDOM.render(
-  <Board />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Board />
+  </React.StrictMode>
 );
